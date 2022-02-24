f69f0ee0bf0d67e7dbd613f275228003ef993034
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-02-24 23:24:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask, send_file, jsonify, request
+from deta import Drive
 
 app = Flask(__name__)
+image_db = Drive("files")
 
 @app.route('/', methods=['GET'])
 def home():
   return "<b> Hello World </b>"
+
+
+#files.get(name)
+#files.list()
+#files.put(file.filename, file.file)
