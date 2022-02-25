fb90d25b26a12cff1cc21bf506333a19138777c6
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-02-25 10:07:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_file , jsonify, request
 from deta import Drive
+from src import mainSERVER
 
 app = Flask(__name__)
 disk = Drive("Disk")
@@ -9,13 +10,18 @@ def home():
   return "<b> Hello World </b>"
 
 
-@app.route('/image', methods=['GET'])
-def image():
+@app.route('/logo/<text>', methods=['GET'])
+def image(text):
   try:
-    return send_file("./assets/IMG_20220224_200511_453.jpg")
+    logo = mainSERVER(True)
+    img = logo.createLogo(text)
+    return send_file(img )
   except Exception as e:
     return str(e)
+  
 
+  
+  
   
 #files.get(name)
 #files.list()
