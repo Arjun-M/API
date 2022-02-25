370274cf31912399522738e12d46c8e9bb36af65
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-02-25 08:47:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, send_file , jsonify, request
 from deta import Drive
 
 app = Flask(__name__)
-image_db = Drive("files")
+disk = Drive("Disk")
 
 @app.route('/', methods=['GET'])
 def home():
@@ -12,10 +12,20 @@ def home():
 @app.route('/image', methods=['GET'])
 def image():
   try:
-    return send_file("/assets/IMG_20220224_200511_453.jpg")
+    return send_file("./assets/IMG_20220224_200511_453.jpg")
   except Exception as e:
     return str(e)
 
+  
+@app.route('/image', methods=['GET'])
+def image():
+  try:
+    f = open( "./assets/IMG_20220224_200511_453.jpg" , 'r')
+    return send_file(f)
+  except Exception as e:
+    return str(e)
+  
+  
 #files.get(name)
 #files.list()
 #files.put(file.filename, file.file)
