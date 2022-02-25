609b3b8d73d7b24aecc760013b39eb6a71069d4a
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-02-25 10:04:29 +0530

    Update src.py

--- src.py
+++ src.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-import json , requests
+import json , requests , random, re
 
 BACKGROUNDS = [
        "./assets/background/A.jpg", "./assets/background/L.jpg", "./assets/background/m.jpg", "./assets/background/n.jpg",
@@ -24,4 +24,19 @@ class mainSERVER:
         self.log = log
         
     def createLogo(self , text ):
-      
+        img = Image.open(random.choice(BACKGROUNDS))
+        draw = ImageDraw.Draw(img)
+        image_widthz, image_heightz = img.size
+        pointsize = 500
+        fillcolor = "gold"
+        shadowcolor = "blue"
+        semx = random.choice(FONTS)
+        font = ImageFont.truetype(semx, 300)
+        w, h = draw.textsize(text, font=font)
+        h += int(h*0.21)
+        image_width, image_height = img.size
+        draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+        x = (image_widthz-w)/2
+        y= ((image_heightz-h)/2+6)
+        draw.text((x, y), text, font=font, fill="white", stroke_width=15, stroke_fill="black")
+        return img
