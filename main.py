d42506c6be6bd9b63a9a013cd8e619cd6dae099d
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-02-25 10:22:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_file , jsonify, request
+from PIL import Image, ImageDraw, ImageFont
 from deta import Drive
-from src import mainSERVER
+from src import mainSERVER , FONTS , BACKGROUNDS
 
 app = Flask(__name__)
 disk = Drive("Disk")
@@ -12,12 +13,22 @@ def home():
 
 @app.route('/logo/<text>', methods=['GET'])
 def image(text):
-  try:
-    logo = mainSERVER(True)
-    img = logo.createLogo(text)
-    return send_file(img , mimetype= "image/jpeg")
-  except Exception as e:
-    return str(e)
+  img = Image.open(random.choice(BACKGROUNDS))
+  draw = ImageDraw.Draw(img)
+  image_widthz, image_heightz = img.size
+  pointsize = 500
+  fillcolor = "gold"
+  shadowcolor = "blue"
+  semx = random.choice(FONTS)
+  font = ImageFont.truetype(semx, 300)
+  w, h = draw.textsize(text, font=font)
+  h += int(h*0.21)
+  image_width, image_height = img.size
+  draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+  x = (image_widthz-w)/2
+  y= ((image_heightz-h)/2+6)
+  draw.text((x, y), text, font=font, fill="white", stroke_width=15, stroke_fill="black")
+  return send_file ( img )        
   
 
   
